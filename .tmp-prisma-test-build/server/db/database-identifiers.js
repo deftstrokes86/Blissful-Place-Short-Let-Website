8395901439f2505021f130c7adbddfda172a423b
Stephen Igwebuike <staicciniventures@gmail.com>
2026-04-07 17:19:57 +0100

    chore(repo): stop tracking local artifacts

--- .tmp-prisma-test-build/server/db/database-identifiers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createDatabaseId = createDatabaseId;
-const node_crypto_1 = require("node:crypto");
-function createDatabaseId(prefix) {
-    return `${prefix}_${(0, node_crypto_1.randomUUID)()}`;
-}
