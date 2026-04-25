ffc0ce63b3c66cae2eeb54a45d38a5767011138c
Stephen Igwebuike <staicciniventures@gmail.com>
2026-04-25 03:18:25 +0100

    fix: update Google Maps embed source for improved location accuracy

--- src/components/site/LocationMap.tsx
+++ src/components/site/LocationMap.tsx
@@ -17,7 +17,7 @@ export function LocationMap({ className, height = "320px" }: LocationMapProps) {
     >
       <iframe
         title="Blissful Place Residences location"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1500!2d3.279664971163975!3d6.677627599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNDAnMzkuNSJOIDPCsDE2JzQ2LjgiRQ!5e0!3m2!1sen!2sng!4v1700000000000!5m2!1sen!2sng"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d400!2d3.279664971163975!3d6.677627599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNDAnMzkuNSJOIDPCsDE2JzQ2LjgiRQ!5e0!3m2!1sen!2sng!4v1700000000000!5m2!1sen!2sng"
         width="100%"
         height="100%"
         style={{ border: 0 }}
